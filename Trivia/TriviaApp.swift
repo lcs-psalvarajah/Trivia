e4b5f324a8f9f02b6a300d8162ad96bc9f28e5f6
jina <psalvarajah@lcs.on.ca>
2021-10-07 09:34:42 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Salvarajah, Prajina on 2021-10-07.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
